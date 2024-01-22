564751a03894cc907b114c95c02c2237cb95af1b
yousef <yousef3425682@gmail.com>
2024-01-22 02:09:47 +0200

    version two

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>version two New Branch</p>
+        <p>version two  New Branch</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
